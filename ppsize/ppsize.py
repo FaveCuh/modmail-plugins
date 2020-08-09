e2ca27f213da3a0b0cc00f6dd3b9f1c7b4f4253f
xTeen <62380638+Teen1@users.noreply.github.com>
2020-08-10 00:24:30 +0100

    Update ppsize.py

--- ppsize/ppsize.py
+++ ppsize/ppsize.py
@@ -1,23 +1,29 @@
 import random
 import discord
 
-@client.command(aliases=['pp'])
-async def penis(ctx, member: discord.Member=None):
-    pps = [
-        '8==D',
-        '8===D',
-        '8====D',
-        '8=====D',
-        '8======D',
-        '8=======D',
-        '8========D',
-        '8=========D'
-    ]
-    if not member:
-        member = ctx.author
-        maincolor = 0xFF4136
-    ppSize = random.choice(pps)
-    embed = discord.Embed(description=f"{ppSize}", color=maincolor)
-    embed.set_author(name=f"{member.display_name}'s Penis:")
-    await ctx.send(embed=embed)
- 
+class pp(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['pp'])
+    async def penis(ctx, member: discord.Member=None):
+        pps = [
+            '8==D',
+            '8===D',
+            '8====D',
+            '8=====D',
+            '8======D',
+            '8=======D',
+            '8========D',
+            '8=========D'
+        ]
+        if not member:
+            member = ctx.author
+            maincolor = 0xFF4136
+        ppSize = random.choice(pps)
+        embed = discord.Embed(description=f"{ppSize}", color=maincolor)
+        embed.set_author(name=f"{member.display_name}'s Penis:")
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(pp(bot))
